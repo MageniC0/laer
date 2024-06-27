8e00484b8370bb528e292f8c44aa225d2eb0932f
MageniC <168565191+MageniC0@users.noreply.github.com>
2024-06-27 13:06:51 +0800

    .

--- laer_alpha0.py
+++ laer_alpha0.py
@@ -58,54 +58,60 @@ for z in range(4):
                 a,b = tr_c[z][y][x]
                 c = e(a)
                 d = e(b)
-                n = tr_n[z][y][x]
+                r = tr_n[z][y][x]
                 for i in range(12):
                     for j in range(12):
                         if bl[i][j]:
                             B[m+i][n+j] = 0
-                if n[4]:
+                if r[4]:
                     for i in range(9):
                         for j in range(4):
                             if up[i][j]:
                                 A[m+i+2][n+j] = a
-                        if n[0]:
+                        if r[0]:
                             for i in range(7):
                                 for j in range(3):
                                     if lp[i][j]:
                                         A[m+i][n+j+2] = b
-                        if n[1]:
+                        if r[1]:
                             for i in range(7):
                                 for j in range(3):
                                     if lp[i][j]:
                                         B[m+i+6][n+j] = 1
-                        if n[2]:
+                        if r[2]:
                             for i in range(7):
                                 for j in range(3):
                                     if rp[i][j]:
                                         A[m+i+6][n+j+2] = b
-                        if n[3]:
+                        if r[3]:
                             for i in range(7):
                                 for j in range(3):
                                     if rp[i][j]:
                                         B[m+i][n+j] = 1
-                if n[5]:
-                    if n[1]:
+                if r[5]:
+                    if r[1]:
                         for i in range(7):
                             for j in range(3):
                                 if lp[i][j]:
                                     B[m+i][n+j+10] = 1
-                    if n[3]:
+                    if r[3]:
                         for i in range(7):
                             for j in range(3):
                                 if rp[i][j]:
                                     B[m+i+6][n+j+10] = 1
-                if n[0]:
+                if r[0]:
                     for i in range(6):
                         for j in range(10):
                             if fl[i][j]:
                                 A[m+i][n+j+3] = a
-                if n[2]:
+                    if r[4] == 1 and r[6] == 1:
+                        for k in range(8):
+                            B[m][n+k+3] = 1
+                if r[2]:
                     for i in range(6):
                         for j in range(10):
                             if fr[i][j]:
                                 A[m+i+7][n+j+3] = a
+                    if r[2] == 1 and r[7] == 1:
+                        for k in range(8):
+                            B[m+12][n+k+3] = 1
